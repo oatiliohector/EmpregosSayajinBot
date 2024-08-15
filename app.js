f55554be220813c851cbd33fdabe6552d78d1c7d
Atilio <atilio.jesus@grupoboticario.com.br>
2024-08-14 22:03:47 -0300

    chore: 🚧 remove estrutura para geracao de pdf

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const PDFDocument = require('pdfkit');
+
 const cors = require('cors');
 
 const app = express();
@@ -10,18 +10,9 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-app.post('/', (req, res) => {
-  const doc = new PDFDocument();
-  let filename = req.body.filename;
-  filename = encodeURIComponent(filename) + '.pdf';
-  res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
-  res.setHeader('Content-type', 'application/pdf');
-  const content = req.body.content;
-  doc.y = 300;
-  doc.text(content, 50, 50);
-  doc.pipe(res);
-  doc.end();
-});
+app.get('/', (req, res) => {
+  res.status(200).send('Hello world!');
+})
 
 app.get('/goodbye', (req, res) => {
   res.status(200).send('Goodbye World\n');
